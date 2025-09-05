3f0b2831a00e02cc3af73f2b064aa381f46ec1ca
jeerola <jaakko.t.eerola@tuni.fi>
2025-09-05 15:21:33 +0300

    Adds homework excuse generator
    Introduces a script to generate humorous excuses for missing homework assignments.
    
    Simplifies input in electric bill calculator to a single line.
    Corrects the formula for calculating the final electric bill amount.

---
+++ 1/TT1/kt1.py
@@ -0,0 +1,8 @@
+startOfMessage = input('Enter the beginning of your excuse (e.g., "Sorry" or "Unfortunately"): ')
+teachersName = input("Enter your teacher's name: ")
+homeworkType = input('What type of homework is missing (e.g., essay, report, etc.)? ')
+petType = input('What kind of animal is your pet? ')
+petName = input("What is your pet's name? ")
+whatPetDid = input('What did your pet do to your homework? ')
+
+print(f"{startOfMessage} {teachersName}, I have no idea where my {homeworkType} went. My pet {petType}, {petName}, must have {whatPetDid} it!")
--- 1/TT1/tt2.py
+++ 1/TT1/tt2.py
@@ -1,5 +1,4 @@
-guessedKwhAmount = input('How much your next month kWh will be?\n')
-kwhAmount = float(guessedKwhAmount)
+kwhAmount = float(input('How much your next month kWh will be?\n'))
 
 baseFee = float(1.79)
 energyFee = float(0.0629)
@@ -7,5 +6,5 @@ transmissionBaseFee = float(3.98)
 transmissionEnergyFee = float(0.031868)
 electricityTax = float(0.0279372)
 
-finalAmount = baseFee + transmissionBaseFee + (energyFee * kwhAmount) + (energyFee * kwhAmount) + (electricityTax * kwhAmount)
+finalAmount = baseFee + transmissionBaseFee + (energyFee * kwhAmount) + (transmissionEnergyFee * kwhAmount) + (electricityTax * kwhAmount)
 print (f'Your next month electric bill will be about {finalAmount:.2f}€')
